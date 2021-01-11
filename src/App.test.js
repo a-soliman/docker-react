d1a998cb4b52adf6fa3dafb713e8a82fb28200e8
Ahmed Soliman <ahmed.soliman@programmer.net>
2021-01-11 11:51:59 -0800

    removed dublicated test case

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,4 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link copy', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
